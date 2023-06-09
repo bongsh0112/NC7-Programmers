7c28494d631a3c032becf4bed3863ed9d6d0817f
SeHwan Bong <bongsh0112@gmail.com>
2023-06-09 20:05:20 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+//    MenOfPassion(A[], n) {
+//      i = ⌊n / 2⌋;
+//      return A[i]; # 코드1
+//    }
+
+    System.out.println(1);
+    System.out.println(0);
+
+  }
+
+}
