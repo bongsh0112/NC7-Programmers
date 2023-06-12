ef4eefaaa829c83ae4452528a6729b7aa7c74b17
SeHwan Bong <bongsh0112@gmail.com>
2023-06-12 12:56:12 +0900

    [unrated] Title: 카운트 다운, Time: 1.99 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public static int[] solution(int start, int end) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = start; i >= end; i--) {
+      list.add(i);
+    }
+    return list.stream().mapToInt(Integer::intValue).toArray();
+  }
+}
