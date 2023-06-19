d8c25699dc6c84968d23e04928aaaec38837c17c
SeHwan Bong <bongsh0112@gmail.com>
2023-06-19 10:04:27 +0900

    [Silver V] Title: 나이순 정렬, Time: 4108 ms, Memory: 177840 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(bufferedReader.readLine());
+    Person[] people = new Person[N];
+
+    for (int i = 0; i < N; i++) {
+      String[] words = bufferedReader.readLine().split(" ");
+      Person p = new Person(Integer.parseInt(words[0]), words[1]);
+      people[i] = p;
+    }
+
+    // 나이순으로 정렬
+    // 나이순으로 정렬
+    Arrays.sort(people, (s1, s2) -> s1.getAge() - s2.getAge());
+
+    for (int i = 0; i < people.length; i++) {
+      System.out.printf("%d %s\n", people[i].getAge(), people[i].getName());
+    }
+  }
+
+  public static class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+      this.age = age;
+      this.name = name;
+    }
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
+
+}
