24f42a4373dec32d6cea4b3dd3df5f9296f17062
SeHwan Bong <bongsh0112@gmail.com>
2023-07-19 14:39:36 +0900

    [unrated] Title: 배열 만들기 6, Time: 17.12 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public static int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+    
+    int[] answer = {};
+    for (int i = 0; i < arr.length; i++) {
+      if (stk.size() == 0) stk.add(arr[i]);
+      else if (stk.get(stk.size() - 1) == arr[i]) stk.remove(stk.size() - 1);
+      else if (stk.get(stk.size() - 1) != arr[i]) stk.add(arr[i]);
+    }
+    
+    if (stk.size() == 0) return new int[]{-1};
+    return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
